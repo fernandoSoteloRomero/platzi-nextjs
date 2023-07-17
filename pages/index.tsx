f991646e4d8f14cbd35a96ad6313d50855bd52e7
fernando sotelo <al169888@alumnos.uacj.mx>
2023-07-17 15:56:49 -0600

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar/Navbar'
 
 const HomePage = () => {
+  const [productList, setproductList] = useState<TProduct[]>([]);
+
+  useEffect(() => {
+    fetch('/api/avo').then(response => response.json()).then(({data,length})=>{
+      setproductList(data)
+    })
+  }, []);
+
   return (
     <div>
       <Navbar />
       <div>Platzi and Next.js!</div>
+      {productList.map(producto =>(
+        <div key={producto.id}>{producto.image}</div>
+      ))}
     </div>
   )
 }
--- pages/product/[id].tsx
+++ pages/product/[id].tsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 
 const ProductPage = () => {
+  const [productWithId, setproductWithId] = useState<TProduct>();
+
   const { query } = useRouter()
+  useEffect(() => {
+    const fetchData = async ()=>{
+      try{
+        const response = await fetch(`/api/avo/${query.id}`)
+        const data = await response.json()
+        setproductWithId(data)
+      }
+      catch(error){
+        console.log(error)
+      }
+    }
+    fetchData()
+  }, [query.id]);
 
   return (
     <section>
       <h1>Página producto: {query.id}</h1>
+      <h2> {productWithId?.name} </h2>
     </section>
   )
 }
